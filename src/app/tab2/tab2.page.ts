7db260f463ad5d0a45165e1d5ae02c8a0b0ce77d
danereid <danereidcode@gmail.com>
2025-09-05 20:24:32 -0400

    re-working app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,66 +2,29 @@ import { Component, OnDestroy } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { AudioRecording } from '../audio-recording';
-import { DomSanitizer } from '@angular/platform-browser';
-
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+  ],
 })
-export class Tab2Page implements OnDestroy {
-  isRecording = false;
-  recordedTime: any;
-  blobUrl: any;
-  test: any;
+export class Tab2Page {
+  posts: string[] = [];
 
-  constructor(
-    private audioRecording: AudioRecording,
-    private sanitizer: DomSanitizer
-  ) {
-    this.audioRecording
-    .recordingFailed()
-    .subscribe(() => (this.isRecording = false));
-    this.audioRecording
-    .getRecordedTime()
-    .subscribe(time => (this.recordedTime = time));
-    this.audioRecording.getRecordedBlob().subscribe(data => {
-      this.test = data;
-      this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(
-        URL.createObjectURL(data.blob)
-      );
-    });
+  constructor(public recorderService: AudioRecording) {}
+
+  async startRecording() {
+    await this.recorderService.startRecording();
   }
 
-  startRecording() {
-    if(!this.isRecording) {
-      this.isRecording = true;
-      this.audioRecording.startRecording();
-    }
+  async stopRecording() {
+    const audioUrl = await this.recorderService.stopRecording();
+    this.posts.unshift(audioUrl); // add to top of timeline
   }
-
-  abortRecording() {
-    if (this.isRecording) {
-      this.isRecording = false;
-      this.audioRecording.abortRecording();
-    }
-  }
-
-  stopRecording() {
-    if (this.isRecording) {
-      this.audioRecording.stopRecording();
-      this.isRecording = false;
-    }
-  }
-
-  clearRecordedData() {
-    this.blobUrl = null;
-  }
-
-  ngOnDestroy(): void {
-    this.abortRecording();
-  }
-
-
 }
